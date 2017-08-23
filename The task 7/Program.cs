6ddaa8bd9e504ad07a3544ac80a3aadd9b2a0335
Владимир Петров <vmpetrov@edu.hse.ru>
2017-08-23 16:52:38 +0500

    Осталось сделать красивый вывод

--- The task 7/Program.cs
+++ The task 7/Program.cs
@@ -10,6 +10,50 @@ namespace The_task_7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите длину кодовых слов: ");
+            bool ok;
+            int SIZE;
+
+            do
+            {
+                ok = Int32.TryParse(Console.ReadLine(), out SIZE);
+                if (!ok) Console.WriteLine("Ошибка ввода данных!");
+                if (SIZE < 1)
+                {
+                    Console.WriteLine("Ошибка длины строки!");
+                    ok = false;
+                }
+            } while (!ok); //ввода размера слов
+
+            List<int>[] mass = new List<int>[SIZE*SIZE];
+            int a = 1;
+            for (int n = 0; n < SIZE * SIZE - 1; n++)
+            {
+                mass[n] = perevod(n);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < (SIZE * SIZE - 1); i++)
+            {
+                mass[i].ForEach(Console.Write);
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<int> perevod(int temp)
+        {
+            int temp1 = 0;
+            List<int> s = new List<int>();
+            while (temp > 0)
+            {
+                temp1 = temp % 2;
+                temp = temp / 2;
+                s.Add(temp1);
+            }
+            return (s);
         }
     }
 }
